bbda881dd2d4d042dee33002b83c83c81516e4e6
Kenan Warren <kenan.warren@escribir.io>
2017-08-10 15:38:59 -0700

    Graphql and graphcool testing

---
+++ hackernews-react-apollo/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import './styles/index.css'
+import { GC_AUTH_TOKEN } from './constants'
+
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj65btgvk08a50108em67eq4w'
+})
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {}
+    }
+    const token = localStorage.getItem(GC_AUTH_TOKEN)
+    req.options.headers.authorization = token ? `Bearer ${token}` : null
+    next()
+  }
+}])
+
+// 3
+const client = new ApolloClient({
+  networkInterface
+})
+
+// 4
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+  , document.getElementById('root')
+)
+registerServiceWorker()
